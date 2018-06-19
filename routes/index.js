439cc9381a6c630f7c4afb7d2782c6e756d44748
nan <50326921@qq.com>
2018-06-19 21:24:49 +0800

    no message

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index.art', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index.art');
 });
+
+router.get('/login', function (req, res, next) {
+  res.render('login.art');
+});
+
+router.get('/register', function (req, res, next) {
+  res.render('register.art');
+});
+
+router.get('/video', function (req, res, next) {
+  res.render('video.art');
+});
+
+router.get('/error404', function (req, res, next) {
+  res.render('error/404.art');
+});
+
+router.get('/error500', function (req, res, next) {
+  res.render('error/500.art');
+});
+
 
 module.exports = router;
